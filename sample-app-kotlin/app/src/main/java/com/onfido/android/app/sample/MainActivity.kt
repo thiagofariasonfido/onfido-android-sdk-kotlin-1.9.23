0c52e0fd234c3d9ce817927a683c40beeb074ce6
Thiago Farias <thiagofarias@mbp-1452.lan>
2024-10-11 16:46:01 +0200

    Initial commit

--- sample-app-kotlin/app/src/main/java/com/onfido/android/app/sample/MainActivity.kt
+++ sample-app-kotlin/app/src/main/java/com/onfido/android/app/sample/MainActivity.kt
@@ -2,70 +2,61 @@ package com.onfido.android.app.sample
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.onfido.android.sdk.capture.ExitCode
-import com.onfido.android.sdk.capture.Onfido
-import com.onfido.android.sdk.capture.OnfidoConfig
-import com.onfido.android.sdk.capture.OnfidoFactory
-import com.onfido.android.sdk.capture.errors.OnfidoException
-import com.onfido.android.sdk.capture.ui.camera.face.stepbuilder.FaceCaptureStepBuilder
-import com.onfido.android.sdk.capture.ui.options.FlowStep
-import com.onfido.android.sdk.capture.upload.Captures
+import com.onfido.workflow.OnfidoWorkflow
+import com.onfido.workflow.WorkflowConfig
+import com.onfido.workflow.OnfidoWorkflow.WorkflowException
 
 class MainActivity : AppCompatActivity() {
-    private var client: Onfido? = null
+
+    private var onfidoWorkflow: OnfidoWorkflow? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
         findViewById<View>(R.id.next).setOnClickListener { startFlow() }
-        client = OnfidoFactory.create(this).client
     }
 
     private fun startFlow() {
-        val flowStepsWithOptions = arrayOf(
-            FlowStep.WELCOME,
-            FlowStep.CAPTURE_DOCUMENT,
-            FaceCaptureStepBuilder.forPhoto().build(),
-            FlowStep.FINAL
-        )
-        startFlow(flowStepsWithOptions)
-    }
+        // TODO: Call your backend to get `sdkToken` and `workflowRunId`
+        val sdkToken =  "eyJhbGciOiJFUzUxMiJ9.eyJleHAiOjE3Mjg2NjAyODMsInBheWxvYWQiOnsiYXBwIjoiYzY1NjEyNjgtNjJhMC00ZGRkLWJiZWEtZGVlNjZlMmMwOGEwIiwiY2xpZW50X3V1aWQiOiJiOTA0NTkzOS0zZjhhLTQ1Y2YtOTJhMi1mMGVhYWQ5MmYxOTAiLCJpc19zYW5kYm94IjpmYWxzZSwiaXNfc2VsZl9zZXJ2aWNlX3RyaWFsIjpmYWxzZSwiaXNfdHJpYWwiOmZhbHNlLCJzYXJkaW5lX3Nlc3Npb24iOiJhYTZiM2QyZC1mZDA5LTQ1ZWUtOTM5NS1kNjE2MDhkOThmODUifSwidXVpZCI6InBsYXRmb3JtX3N0YXRpY19hcGlfdG9rZW5fdXVpZCIsImVudGVycHJpc2VfZmVhdHVyZXMiOnsiY29icmFuZCI6dHJ1ZSwibG9nb0NvYnJhbmQiOnRydWUsInVzZUN1c3RvbWl6ZWRBcGlSZXF1ZXN0cyI6dHJ1ZSwidmFsaWRDcm9zc0RldmljZVVybHMiOlsiIl19LCJ1cmxzIjp7ImRldGVjdF9kb2N1bWVudF91cmwiOiJodHRwczovL3Nkay5vbmZpZG8uY29tIiwic3luY191cmwiOiJodHRwczovL3N5bmMub25maWRvLmNvbSIsImhvc3RlZF9zZGtfdXJsIjoiaHR0cHM6Ly9pZC5vbmZpZG8uY29tIiwiYXV0aF91cmwiOiJodHRwczovL2FwaS5vbmZpZG8uY29tIiwib25maWRvX2FwaV91cmwiOiJodHRwczovL2FwaS5vbmZpZG8uY29tIiwidGVsZXBob255X3VybCI6Imh0dHBzOi8vYXBpLm9uZmlkby5jb20ifX0.MIGIAkIAovsXbdJiu23fALMomhADnHgfkKNGF0kb5QPId0GvAzqcwG_8XfQHpoTbATcTMuHV-RJwCPBEdXT_4fdam2EsoQACQgDDNOp1HU3_v_faLmAZ_A-w7Qo9pxFLUHGSX-nu5_GsNFmBMRdllgeA_i2V4CbiBmcH5j-9ihPnEPMNgQqV8SrcSQ"
+        val workflowRunId = "8c9aeb47-68ee-4137-9be1-0a30adeecbb4"
 
-    private fun startFlow(flowSteps: Array<FlowStep>) {
-        // TODO Call your backend to get `sdkToken` https://github.com/onfido/onfido-android-sdk#41-sdk-token
-        val sdkToken = "YOUR_SDK_TOKEN"
-        val onfidoConfig = OnfidoConfig.builder(this@MainActivity)
-            .withSDKToken(sdkToken)
-            .withCustomFlow(flowSteps)
-            .build()
-        client!!.startActivityForResult(this@MainActivity, 1, onfidoConfig)
+        val workflowConfig = WorkflowConfig.Builder(sdkToken, workflowRunId).build()
+        onfidoWorkflow = OnfidoWorkflow.create(this)
+
+        startActivityForResult(onfidoWorkflow!!.createIntent(workflowConfig), 1)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        client!!.handleActivityResult(resultCode, data, object : Onfido.OnfidoResultListener {
-            override fun userExited(exitCode: ExitCode) {
-                showToast("User cancelled.")
+
+        onfidoWorkflow?.handleActivityResult(resultCode, data, object : OnfidoWorkflow.ResultListener {
+            override fun onUserCompleted() {
+                // Called when the entire workflow run has reached the terminal node.
+                showToast("User completed the flow.")
             }
 
-            override fun userCompleted(captures: Captures) {
-                startCheck(captures)
+            override fun onUserExited(exitCode: ExitCode) {
+                // Called when the user has exited the flow prematurely.
+                showToast("User cancelled the flow.")
+                Log.d("Onfido", "User exited with exitCode: $exitCode")
             }
 
-            override fun onError(exception: OnfidoException) {
+            override fun onException(exception: WorkflowException) {
+                // Called when the flow has ended with an exception
                 exception.printStackTrace()
                 showToast("Unknown error")
+                Log.d("Onfido", "Exception: ${exception.message}")
             }
         })
     }
 
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-    }
-
-    private fun startCheck(captures: Captures) {
-        //Call your back end to initiate the check https://github.com/onfido/onfido-android-sdk#2-creating-a-check
     }
 }
--- sample-app/app/src/main/java/com/onfido/android/app/sample/MainActivity.java
+++ sample-app/app/src/main/java/com/onfido/android/app/sample/MainActivity.java
@@ -1,88 +1,57 @@
-package com.onfido.android.app.sample;
+//This script is to implement Onfido Studio in Androd using Java language.
 
+package com.onfido.android.app.sample;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.onfido.android.sdk.capture.ExitCode;
 import com.onfido.android.sdk.capture.Onfido;
-import com.onfido.android.sdk.capture.OnfidoConfig;
 import com.onfido.android.sdk.capture.OnfidoFactory;
-import com.onfido.android.sdk.capture.errors.OnfidoException;
-import com.onfido.android.sdk.capture.ui.camera.face.stepbuilder.FaceCaptureStepBuilder;
-import com.onfido.android.sdk.capture.ui.options.FlowStep;
-import com.onfido.android.sdk.capture.upload.Captures;
+import com.onfido.workflow.OnfidoWorkflow;
+import com.onfido.workflow.WorkflowConfig;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Onfido client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.main);
-
         findViewById(R.id.next).setOnClickListener(v -> startFlow());
-
-        client = OnfidoFactory.create(this).getClient();
     }
-
+    OnfidoWorkflow onfidoWorkflow;
     private void startFlow() {
-        final FlowStep[] flowStepsWithOptions = new FlowStep[]{
-                FlowStep.WELCOME,
-                FlowStep.CAPTURE_DOCUMENT,
-                FaceCaptureStepBuilder.forPhoto().build(),
-                FlowStep.FINAL
-        };
-
-        startFlow(flowStepsWithOptions);
+        String sdkToken = "eyJhbGciOiJFUzUxMiJ9.eyJleHAiOjE3Mjg2NTUwMDUsInBheWxvYWQiOnsiYXBwIjoiYzY1NjEyNjgtNjJhMC00ZGRkLWJiZWEtZGVlNjZlMmMwOGEwIiwiY2xpZW50X3V1aWQiOiJiOTA0NTkzOS0zZjhhLTQ1Y2YtOTJhMi1mMGVhYWQ5MmYxOTAiLCJpc19zYW5kYm94IjpmYWxzZSwiaXNfc2VsZl9zZXJ2aWNlX3RyaWFsIjpmYWxzZSwiaXNfdHJpYWwiOmZhbHNlLCJzYXJkaW5lX3Nlc3Npb24iOiIwMDZjNTYzYi0wODc5LTQ0MzItYTc2YS1iNmZjNWUwMjcyMzQifSwidXVpZCI6InBsYXRmb3JtX3N0YXRpY19hcGlfdG9rZW5fdXVpZCIsImVudGVycHJpc2VfZmVhdHVyZXMiOnsiY29icmFuZCI6dHJ1ZSwibG9nb0NvYnJhbmQiOnRydWUsInVzZUN1c3RvbWl6ZWRBcGlSZXF1ZXN0cyI6dHJ1ZSwidmFsaWRDcm9zc0RldmljZVVybHMiOlsiIl19LCJ1cmxzIjp7ImRldGVjdF9kb2N1bWVudF91cmwiOiJodHRwczovL3Nkay5vbmZpZG8uY29tIiwic3luY191cmwiOiJodHRwczovL3N5bmMub25maWRvLmNvbSIsImhvc3RlZF9zZGtfdXJsIjoiaHR0cHM6Ly9pZC5vbmZpZG8uY29tIiwiYXV0aF91cmwiOiJodHRwczovL2FwaS5vbmZpZG8uY29tIiwib25maWRvX2FwaV91cmwiOiJodHRwczovL2FwaS5vbmZpZG8uY29tIiwidGVsZXBob255X3VybCI6Imh0dHBzOi8vYXBpLm9uZmlkby5jb20ifX0.MIGIAkIAuA27xjEyjT1U_pJEjAfwqWSGMDhRsREW16KJ6_wmXMVYK99eU52H4USu0gmsIlCMHXh4N3oryTFePCwSOzrTowACQgF5wk1q3ws1vS3jjcNyN9AUu3sLYHp-tN2FyzbyH57kDAU1shDVinS2J1cLu72MIqXhqVwxH-6If78ldnu2Haijjw";
+        String workflowRunId = "d8fde377-9972-495c-99d4-7b27762e007a";
+        WorkflowConfig workflowConfig = new WorkflowConfig.Builder(sdkToken, workflowRunId).build();
+        onfidoWorkflow = OnfidoWorkflow.create(this);
+        startActivityForResult(onfidoWorkflow.createIntent(workflowConfig), 1);
     }
-
-    private void startFlow(final FlowStep[] flowSteps) {
-        // TODO Call your backend to get `sdkToken` https://github.com/onfido/onfido-android-sdk#41-sdk-token
-        String sdkToken = "YOUR_SDK_TOKEN";
-
-        OnfidoConfig onfidoConfig =
-                OnfidoConfig.builder(MainActivity.this)
-                        .withSDKToken(sdkToken)
-                        .withCustomFlow(flowSteps)
-                        .build();
-
-        client.startActivityForResult(MainActivity.this, 1, onfidoConfig);
-    }
-
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        client.handleActivityResult(resultCode, data, new Onfido.OnfidoResultListener() {
+        onfidoWorkflow.handleActivityResult(resultCode, data, new OnfidoWorkflow.ResultListener() {
             @Override
-            public void userExited(@NonNull ExitCode exitCode) {
-                showToast("User cancelled.");
+            public void onUserCompleted() {
+                // Called when the entire workflow run has reached the terminal node.
             }
-
             @Override
-            public void userCompleted(@NonNull Captures captures) {
-                startCheck(captures);
+            public void onUserExited(ExitCode exitCode) {
+                // Called when the user has exited the flow prematurely.
+                showToast("User cancelled the flow.");
+                Log.d("Testing", "onUserExited: $exitCode");
             }
-
             @Override
-            public void onError(OnfidoException e) {
-                e.printStackTrace();
+            public void onException(OnfidoWorkflow.WorkflowException exception) {
+                // Called when the flow has ended with an exception
+                exception.printStackTrace();
                 showToast("Unknown error");
+                Log.d("Testing", "onException: $exception");
             }
         });
     }
-
     private void showToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-    }
-
-    private void startCheck(Captures captures) {
-        //Call your back end to initiate the check https://github.com/onfido/onfido-android-sdk#2-creating-a-check
     }
 }
